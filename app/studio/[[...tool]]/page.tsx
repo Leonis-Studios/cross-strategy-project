81bfb13886d2bbc7e102417d59f0cde042be6fac
Hassan Shirazi <66129579+Nyqvuist@users.noreply.github.com>
2026-04-23 20:56:02 -0400

    whoops slight change to page.tsx

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,8 @@
-import { NextStudio } from 'next-sanity/studio'
-import config from '@/sanity.config'
+import { NextStudio } from "next-sanity/studio";
+import config from "@/sanity.config";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <NextStudio config={config} />;
 }
